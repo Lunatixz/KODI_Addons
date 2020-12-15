1d76d73e797c19ba8dafffd9cb4384b0d7543c7a
Lunatixz <Kgraer@gmail.com>
2020-12-15 17:40:10 -0500

    NewsOn v.1.2.0

--- plugin.video.newson/resources/lib/newson.py
+++ plugin.video.newson/resources/lib/newson.py
@@ -18,17 +18,20 @@
 
 # -*- coding: utf-8 -*-
 import os, sys, time, datetime, traceback, feedparser, random
-import urlparse, urllib, urllib2, socket, json, collections, gzip
+import socket, json, collections, gzip
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon, xbmcvfs
 
-from StringIO import StringIO
-from simplecache import SimpleCache, use_cache
+from io            import BytesIO
+from simplecache   import SimpleCache, use_cache
+from six.moves     import urllib
+from kodi_six      import xbmc, xbmcaddon, xbmcplugin, xbmcgui, xbmcvfs, py2_encode, py2_decode
+
 # Plugin Info
 ADDON_ID      = 'plugin.video.newson'
 REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
 ADDON_NAME    = REAL_SETTINGS.getAddonInfo('name')
 SETTINGS_LOC  = REAL_SETTINGS.getAddonInfo('profile')
-ADDON_PATH    = REAL_SETTINGS.getAddonInfo('path').decode('utf-8')
+ADDON_PATH    = REAL_SETTINGS.getAddonInfo('path')
 ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
 ICON          = REAL_SETTINGS.getAddonInfo('icon')
 FANART        = REAL_SETTINGS.getAddonInfo('fanart')
@@ -44,11 +47,14 @@ DEBUG         = REAL_SETTINGS.getSetting('Enable_Debugging') == 'true'
 BASE_API      = 'http://watchnewson.com/api/linear/channels'
 LOGO_URL      = 'https://dummyimage.com/512x512/035e8b/FFFFFF.png&text=%s'
 FAN_URL       = 'https://dummyimage.com/1280x720/035e8b/FFFFFF.png&text=%s'
-MAP_URL       = 'https://www.mapquestapi.com/staticmap/v5/map?key=%s&center=%s&size=1280,720&zoom=14&type=dark'
+MAP_URL       = 'https://www.mapquestapi.com/staticmap/v5/map?key=%s&center=%s&size=@2x'
+
 MENU          = [(LANGUAGE(30002), '0', 0, False, {"thumb":NEWSART,"poster":NEWSART,"fanart":FANART,"icon":ICON,"logo":ICON}),
                  (LANGUAGE(30003), '2', 2, False, {"thumb":CLIPART,"poster":CLIPART,"fanart":FANART,"icon":ICON,"logo":ICON})]
            
 def log(msg, level=xbmc.LOGDEBUG):
+    try: msg = str(msg)
+    except: pass
     if DEBUG == False and level != xbmc.LOGERROR: return
     if level == xbmc.LOGERROR: msg += ' ,' + traceback.format_exc()
     xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + msg, level)
@@ -56,7 +62,7 @@ def log(msg, level=xbmc.LOGDEBUG):
 socket.setdefaulttimeout(TIMEOUT)
 class NewsOn(object):
     def __init__(self, sysARG):
-        log('__init__, sysARG = ' + str(sysARG))
+        log('__init__, sysARG = %s'%(sysARG))
         self.sysARG    = sysARG
         self.cache     = SimpleCache()
         self.stateMenu = self.getStates()
@@ -64,29 +70,33 @@ class NewsOn(object):
 
     def getMAP(self, args):
         try:
-            if len(args) == 2: return MAP_URL%(APIKEY, '%s,%s'%(tuple(args)))
-            else: return MAP_URL%(APIKEY, args)
+            map = self.cache.get(ADDON_NAME + '.getMAPs, args = %s'%str(args))
+            if not map:
+                if len(args) == 2: map = MAP_URL%(APIKEY, '%s,%s'%(tuple(args)))
+                else: map = MAP_URL%(APIKEY, urllib.parse.quote(args))
+                self.cache.set(ADDON_NAME + '.getMAPs, args = %s'%str(args), map, expiration=datetime.timedelta(days=28))
+            return map
         except: return FANART
             
         
     def openURL(self, url):
         try:
-            log('openURL, url = ' + str(url))
-            if DEBUG: cacheresponse = None
-            else: cacheresponse = self.cache.get(ADDON_NAME + '.openURL, url = %s'%url)
+            log('openURL, url = %s'%(url))
+            cacheresponse = self.cache.get(ADDON_NAME + '.openURL, url = %s'%url)
             if not cacheresponse:
-                request = urllib2.Request(url)
+                request = urllib.request.Request(url)
                 request.add_header('Accept-Encoding', 'gzip')
                 request.add_header('User-Agent','Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)')
-                response = urllib2.urlopen(request, timeout = TIMEOUT)
-                if response.info().get('Content-Encoding') == 'gzip': cacheresponse = gzip.GzipFile(fileobj=StringIO(response.read())).read()
-                else: cacheresponse = response
+                response = urllib.request.urlopen(request, timeout = TIMEOUT)
+                if response.info().get('Content-Encoding') == 'gzip': 
+                    cacheresponse = gzip.GzipFile(fileobj=BytesIO(response.read())).read()
+                else: 
+                    cacheresponse = response
                 response.close()
                 self.cache.set(ADDON_NAME + '.openURL, url = %s'%url, cacheresponse, expiration=datetime.timedelta(minutes=5))
-            if isinstance(cacheresponse, basestring): cacheresponse = json.loads(cacheresponse)
-            return cacheresponse
+            return json.loads(cacheresponse)
         except Exception as e: 
-            log("openURL Failed! " + str(e), xbmc.LOGERROR)
+            log("openURL Failed! %s"%(e), xbmc.LOGERROR)
             xbmcgui.Dialog().notification(ADDON_NAME, LANGUAGE(30001), ICON, 4000)
             return ''
         
@@ -97,7 +107,7 @@ class NewsOn(object):
         
                     
     def browseMenu(self, id=1):
-        log('browseMenu, id = ' + str(id))
+        log('browseMenu, id = %s'%(id))
         self.stateMenu = [tuple(s.format(id) for s in tup) for tup in self.stateMenu]
         for item in self.stateMenu: 
             self.addDir(item[0], item[1], item[2], False, {"thumb":LOGO_URL%item[0],"poster":LOGO_URL%item[0],"fanart":self.getMAP((item[0])),"landscape":FAN_URL%(item[0]),"icon":ICON,"logo":ICON})
@@ -114,7 +124,7 @@ class NewsOn(object):
         data = self.openURL(BASE_API)
         if len(data) == 0: return []
         for channel in data:
-            try: state.append(self.cleanState(channel['config']['state']))
+            try:    state.append(self.cleanState(channel['config']['state']))
             except: state.append(self.cleanState(channel['config']['locations'][0]['state']))
         states = collections.Counter(state)
         for key, value in sorted(states.items()): stateLST.append(("%s"%(key), key , '{}'))
@@ -122,7 +132,7 @@ class NewsOn(object):
             
             
     def newsCasts(self, state):
-        log('newsCasts, state = ' + state)
+        log('newsCasts, state = %s'%(state))
         urls = []
         data = self.openURL(BASE_API)
         if len(data) == 0: return
@@ -130,7 +140,7 @@ class NewsOn(object):
             try: states = self.cleanState(channel['config']['state'])
             except: states = self.cleanState(channel['config']['locations'][0]['state'])
             if state in states:
-                chid   = channel['identifier']
+                chid   = str(channel['identifier'])
                 title  = channel['title']
                 lat    = channel['config']['latitude']
                 lon    = channel['config']['longitude']
@@ -150,7 +160,7 @@ class NewsOn(object):
         
         
     def videoclips(self, state):
-        log('videoclips, state = ' + state)
+        log('videoclips, state = %s'%(state))
         data = self.openURL(BASE_API)
         if len(data) == 0: return
         for channel in data:
@@ -159,7 +169,7 @@ class NewsOn(object):
             if state in states:
                 try: 
                     vidURL = channel['config']['localvodfeed']
-                    chid   = channel['identifier']
+                    chid   = str(channel['identifier'])
                     title  = channel['title']
                     lat    = channel['config']['latitude']
                     lon    = channel['config']['longitude']
@@ -173,7 +183,7 @@ class NewsOn(object):
 
     def parseclips(self, url):
         if url is None: return
-        log('parseclips, url = ' + url)
+        log('parseclips, url = %s'%(url))
         feed = feedparser.parse(url)
         for item in feed['entries']:
             if item and 'summary_detail' in item:
@@ -201,50 +211,49 @@ class NewsOn(object):
     def playVideo(self, name, url, live=False):
         log('playVideo')
         liz = xbmcgui.ListItem(name, path=url)
+        liz.setProperty('IsPlayable','true')
+        liz.setProperty('IsInternetStream','true')
         xbmcplugin.setResolvedUrl(int(self.sysARG[1]), True, liz)
 
            
     def addLink(self, name, u, mode, infoList=False, infoArt=False, total=0):
-        name = name.encode("utf-8")
-        log('addLink, name = ' + name)
+        log('addLink, name = %s'%(name))
         liz=xbmcgui.ListItem(name)
-        liz.setProperty('IsPlayable', 'true')
+        liz.setProperty('IsPlayable','true')
+        liz.setProperty('IsInternetStream','true')
         if infoList == False: liz.setInfo( type="Video", infoLabels={"mediatype":"video","label":name,"title":name})
         else: liz.setInfo(type="Video", infoLabels=infoList)
         if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
         else: liz.setArt(infoArt)
-        u=self.sysARG[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        u=self.sysARG[0]+"?url="+urllib.parse.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.parse.quote_plus(name)
         xbmcplugin.addDirectoryItem(handle=int(self.sysARG[1]),url=u,listitem=liz,totalItems=total)
 
 
     def addDir(self, name, u, mode, infoList=False, infoArt=False):
-        name = name.encode("utf-8")
-        log('addDir, name = ' + name)
+        log('addDir, name = %s'%(name))
         liz=xbmcgui.ListItem(name)
         liz.setProperty('IsPlayable', 'false')
         if infoList == False: liz.setInfo(type="Video", infoLabels={"mediatype":"video","label":name,"title":name})
         else: liz.setInfo(type="Video", infoLabels=infoList)
         if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
         else: liz.setArt(infoArt)
-        u=self.sysARG[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        u=self.sysARG[0]+"?url="+urllib.parse.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.parse.quote_plus(name)
         xbmcplugin.addDirectoryItem(handle=int(self.sysARG[1]),url=u,listitem=liz,isFolder=True)
         
         
     def getParams(self):
-        return dict(urlparse.parse_qsl(self.sysARG[2][1:]))
+        return dict(urllib.parse.parse_qsl(self.sysARG[2][1:]))
 
             
     def run(self):  
         params=self.getParams()
-        try: url=urllib.unquote_plus(params["url"])
+        try:    url=urllib.parse.unquote_plus(params["url"])
         except: url=None
-        try: name=urllib.unquote_plus(params["name"])
+        try:    name=urllib.parse.unquote_plus(params["name"])
         except: name=None
-        try: mode=int(params["mode"])
+        try:    mode=int(params["mode"])
         except: mode=None
-        log("Mode: "+str(mode))
-        log("URL : "+str(url))
-        log("Name: "+str(name))
+        log("Mode: %s, URL : %s, Name: %s"%(mode,url,name))
 
         if mode==None:  self.mainMenu()
         elif mode == 0: self.browseMenu(1)

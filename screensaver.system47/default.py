e173b94c817f161f34a70521ff2f8bf4ff60c1ab
Lunatixz <128316+Lunatixz@users.noreply.github.com>
2023-08-10 17:17:14 -0400

    System47 Saver v.2.5.01

---
+++ screensaver.system47/default.py
@@ -0,0 +1,122 @@
+#   Copyright (C) 2023 Lunatixz
+#
+#
+# This file is part of System 47 Live in HD Screensaver.
+#
+# System 47 Live in HD Screensaver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# System 47 Live in HD Screensaver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with System 47 Live in HD Screensaver.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, random, traceback, json, base64
+# import youtube_registration # https://github.com/jdf76/plugin.video.youtube/issues/184
+
+from kodi_six    import xbmc, xbmcaddon, xbmcplugin, xbmcgui, xbmcvfs
+
+
+# youtube_registration.register_api_keys(addon_id=ADDON_ID,
+                                       # api_key=base64.urlsafe_b64decode(REAL_SETTINGS.getSetting('AKEY')),
+                                       # client_id= base64.urlsafe_b64decode(REAL_SETTINGS.getSetting('CKEY')),
+                                       # client_secret=base64.urlsafe_b64decode(REAL_SETTINGS.getSetting('SKEY')))
+            
+# Plugin Info
+ADDON_ID      = 'screensaver.system47'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME    = REAL_SETTINGS.getAddonInfo('name')
+SETTINGS_LOC  = REAL_SETTINGS.getAddonInfo('profile')
+ADDON_PATH    = REAL_SETTINGS.getAddonInfo('path')
+ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
+ICON          = REAL_SETTINGS.getAddonInfo('icon')
+FANART        = REAL_SETTINGS.getAddonInfo('fanart')
+LANGUAGE      = REAL_SETTINGS.getLocalizedString
+
+## GLOBALS ##
+ACTION_STOP   = 13
+MUTE          = REAL_SETTINGS.getSetting('Enable_Mute') == 'true'
+RANDOM        = REAL_SETTINGS.getSetting('Enable_Random') == 'true'
+YTID          = 'N9Sxyvz4lW4'
+FILENAME      = 'screensaver.system47.v.2.5.01.mp4'
+ 
+def log(msg, level = xbmc.LOGDEBUG):
+    if level == xbmc.LOGERROR: msg += ' ,' + traceback.format_exc()
+    xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + (msg), xbmc.LOGERROR)
+     
+def getURL():
+    if xbmc.getCondVisibility('System.HasAddon(plugin.video.youtube)') and xbmc.getCondVisibility('System.AddonIsEnabled(plugin.video.youtube)'):
+        return 'plugin://plugin.video.youtube/play/?video_id=%s&suggested=false&incognito=true'%(YTID)
+    elif xbmc.getCondVisibility('System.HasAddon(plugin.video.tubed)') and xbmc.getCondVisibility('System.AddonIsEnabled(plugin.video.tubed)'):
+        return 'plugin://plugin.video.tubed/?mode=play&video_id=%s'%(YTID)
+    else:
+        xbmcgui.Dialog().notification(ADDON_NAME, LANGUAGE(30004), ICON, 4000)
+       
+def isMute():
+    state = False
+    json_query = '{"jsonrpc":"2.0","method":"Application.GetProperties","params":{"properties":["muted"]},"id":1}'
+    json_response = json.loads(xbmc.executeJSONRPC(json_query))
+    if json_response and 'result' in json_response: state = json_response['result']['muted']
+    log('isMute, state = ' + str(state))
+    return state
+    
+def setMute(state):
+    log('setMute, state = ' + str(state))
+    if isMute() == state: return
+    json_query = '{"jsonrpc":"2.0","method":"Application.SetMute","params":{"mute":%s},"id":1}'%str(state).lower()
+    json_response = json.loads(xbmc.executeJSONRPC(json_query))
+    
+    
+class Player(xbmc.Player):
+    def onAVStarted(self):
+        log('onAVStarted')
+        totalTime = self.getTotalTime()
+        seekValue = int(totalTime//(random.randint(5,(totalTime//4)))) if RANDOM else 0
+        xbmc.Monitor().waitForAbort(5.0)
+        if seekValue > 5.0: xbmc.executebuiltin('Seek(%s)'%seekValue)
+
+        
+class BackgroundWindow(xbmcgui.WindowXMLDialog):
+    def __init__(self, *args, **kwargs):
+        self.myPlayer = Player()
+        self.myPlayer.Background = self
+        if MUTE: setMute(True)
+    
+    
+    def playFile(self):
+        if xbmcvfs.exists(os.path.join(SETTINGS_LOC,FILENAME)):
+            return os.path.join(SETTINGS_LOC,FILENAME)
+        else:
+            return getURL()
+        
+        
+    def onInit(self):
+        xbmc.executebuiltin("PlayerControl(repeatone)")
+        # xbmcgui.Window(10000).setProperty('plugin.video.youtube-configs', CONFIGS)
+        self.myPlayer.play(self.playFile())
+    
+        
+    def onAction(self, act):
+        self.closeBackground()
+        
+        
+    def closeBackground(self):
+        log('closeBackground')
+        self.myPlayer.stop()
+        # xbmcgui.Window(10000).clearProperty('plugin.video.youtube-configs')
+        xbmc.executebuiltin("PlayerControl(repeatoff)")
+        if MUTE: setMute(False)
+        self.close()
+        
+      
+class Start(object):
+    def __init__(self):
+        self.background = BackgroundWindow('%s.background.xml'%ADDON_ID, ADDON_PATH, "Default", windowed=True)
+        self.background.doModal()
+        
+if __name__ == '__main__': Start()
---
+++ screensaver.system47/download.py
@@ -0,0 +1,137 @@
+#   Copyright (C) 2023 Lunatixz
+#
+#
+# This file is part of System 47 Live in HD Screensaver.
+#
+# System 47 Live in HD Screensaver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# System 47 Live in HD Screensaver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with System 47 Live in HD Screensaver.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, time, traceback, requests, re, sys
+import shutil, tempfile, six
+
+from kodi_six  import xbmc, xbmcaddon, xbmcplugin, xbmcgui, xbmcvfs
+from six.moves import urllib
+from bs4       import BeautifulSoup
+    
+# Plugin Info
+ADDON_ID      = 'screensaver.system47'
+REAL_SETTINGS = xbmcaddon.Addon(id=ADDON_ID)
+ADDON_NAME    = REAL_SETTINGS.getAddonInfo('name')
+ADDON_PATH    = REAL_SETTINGS.getAddonInfo('path')
+SETTINGS_LOC  = REAL_SETTINGS.getAddonInfo('profile')
+ADDON_VERSION = REAL_SETTINGS.getAddonInfo('version')
+ICON          = REAL_SETTINGS.getAddonInfo('icon')
+FANART        = REAL_SETTINGS.getAddonInfo('fanart')
+LANGUAGE      = REAL_SETTINGS.getLocalizedString
+DEBUG         = True
+FILENAME      = 'screensaver.system47.v.2.5.01.mp4'
+FILEPATH      = xbmcvfs.translatePath(os.path.join(SETTINGS_LOC,FILENAME))
+# DOWNLOAD_URL  = 'http://www.mediafire.com/file/cvptnk5p5zk41zb/screensaver.system47.mp4/file'
+DOWNLOAD_URL  = 'https://www.mediafire.com/file/xjkqyiy9for97fl/screensaver.system47.v.2.5.01.mp4/file'
+CHUNK_SIZE    = 512 * 1024  # 512KB
+
+def log(msg, level=xbmc.LOGDEBUG):
+    if DEBUG == False and level != xbmc.LOGERROR: return
+    if level == xbmc.LOGERROR: msg += ' ,' + traceback.format_exc()
+    xbmc.log(ADDON_ID + '-' + ADDON_VERSION + '-' + (msg), level)
+
+class Download(object):
+    def __init__(self):
+        self.start(DOWNLOAD_URL,FILEPATH)
+        
+ 
+    def start(self, url, output, quiet=False):
+        url_origin = url
+        sess = requests.session()
+        sess.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.178 Safari/537.36"
+        }
+            
+        while not xbmc.Monitor().abortRequested():
+            res = sess.get(url, stream=True)
+            if 'Content-Disposition' in res.headers:
+                break
+                
+            for line in res.text.splitlines():
+                m = re.search(r'href="((http|https)://download[^"]+)', line)
+                if m:
+                    url = m.groups()[0]
+                    break
+                    
+            if url is None:
+                return log('start, Permission denied: %s'%url_origin)
+                
+        if output is None:
+            m = re.search(
+                'filename="(.*)"', res.headers['Content-Disposition']
+            )
+            output = m.groups()[0]
+            output = os.path.join(SETTINGS_LOC,output.encode('iso8859').decode('utf-8'))
+        output_is_path = isinstance(output, six.string_types)
+
+        if not quiet:
+            msg = '%s\n%s\n%s'%(LANGUAGE(30002),FILENAME,FILEPATH)
+
+        if output_is_path:
+            tmp_file = tempfile.mktemp(
+                suffix=tempfile.template,
+                prefix=os.path.basename(output),
+                dir=os.path.dirname(output),
+            )
+            f = open(tmp_file, 'wb')
+        else:
+            tmp_file = None
+            f = output
+
+        try:
+            total = res.headers.get('Content-Length')
+            if total is not None:
+                total = int(total)
+            if not quiet:
+                dia = xbmcgui.DialogProgress()
+                dia.create(ADDON_NAME,msg)
+                dia.update(0, msg)
+                
+            for idx, chunk in enumerate(res.iter_content(chunk_size=CHUNK_SIZE)):
+                f.write(chunk)
+                if not quiet:
+                    if dia.iscanceled():
+                        raise Exception('Download Canceled')
+                    per = int(idx*100//(total//CHUNK_SIZE))
+                    msg = '%s (%s%%)\n%s\n%s'%(LANGUAGE(30002),per,FILENAME,FILEPATH)
+                    dia.update(per,msg)
+                    
+            if not quiet:
+                dia.close()
+                
+            if tmp_file:
+                f.close()
+                shutil.move(tmp_file, output)
+        except Exception as e:
+            log("start, failed! %s"%(e), xbmc.LOGERROR)
+            xbmcgui.Dialog().notification(ADDON_NAME, LANGUAGE(30001), ICON, 4000)
+            return self.deletefiles(tmp_file)
+        finally: self.deletefiles(tmp_file)
+        return output
+
+        
+    def deletefiles(self, tmp_file):
+        #todo clean up all files ending with tmp
+        try:
+            if tmp_file:
+                os.remove(tmp_file)
+        except OSError:
+            pass
+            
+if __name__ == '__main__':
+    Download()
